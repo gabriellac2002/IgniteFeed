cd95f4586bf0d2378a3ef183ac302fcb7cb5890e
gabriellac2002 <gabriellacsilva2002@gmail.com>
2024-12-10 09:28:11 -0300

    add logo SVG and integrate it into Header component

--- ignite-feed/src/components/Header.tsx
+++ ignite-feed/src/components/Header.tsx
@@ -1,6 +1,9 @@
+import logo from "../assets/logo.svg";
+
 export default function Header() {
   return (
-    <header className="bg-gray-800">
+    <header className="bg-gray-800 flex justify-center items-center gap-2 px-0 py-5">
+      <img src={logo} alt="Ignite Feed" className="h-8" />
       <h1>Ignite Feed</h1>
     </header>
   );
