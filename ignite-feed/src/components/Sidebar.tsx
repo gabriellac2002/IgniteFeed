cc4c8d2804dda1394eb6ad773c6ebf5317f7ac83
gabriellac2002 <gabriellacsilva2002@gmail.com>
2024-12-10 10:52:16 -0300

    add react-icons dependency and update Sidebar component with edit icon and styling adjustments

--- ignite-feed/src/components/Sidebar.tsx
+++ ignite-feed/src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { RiEdit2Fill } from "react-icons/ri";
+
 export default function Sidebar() {
   return (
     <aside className="bg-gray-800 rounded-lg overflow-hidden">
@@ -7,22 +9,24 @@ export default function Sidebar() {
         className="w-full h-20 object-cover"
       />
 
-      <div className="flex flex-col items-center mt-[calc(0px-1.5rem-4px)]">
+      <div className="flex flex-col items-center mt-[calc(0px-2rem-4px)]">
         <img
           src="https://github.com/gabriellac2002.png"
-          className="
-          rounded-lg border-4 border-gray-800 outline outline-2 outline-green-500"
-          style={{ height: `calc(3rem + 8px)`, width: `calc(3rem + 8px)` }}
+          className="rounded-lg border-4 border-gray-800 outline outline-2 outline-green-500"
+          style={{ height: `calc(4rem + 8px)`, width: `calc(4rem + 8px)` }}
         />
-        <strong className="mt-4 text-gray-100 mb-1">Gabriella Cruz</strong>
+        <strong className="mt-4 text-base text-gray-100 mb-1">
+          Gabriella Cruz
+        </strong>
         <span className="text-sm text-gray-400">Front-end developer</span>
       </div>
 
       <footer className="border-t-2 border-t-gray-600 mt-6 px-8 pb-8 pt-6">
         <a
           href="#"
-          className="w-full bg-transparent text-green-500 border border-green-500 rounded-lg h-12 p-6 font-bold flex justify-center items-center"
+          className="w-full bg-transparent text-green-500 border border-green-500 rounded-lg h-12 py-6 px-0 font-bold flex justify-center items-center gap-2 hover:bg-green-500 hover:text-white transition-colors duration-200"
         >
+          <RiEdit2Fill className="w-[20px] h-[20px]" />
           Editar seu perfil
         </a>
       </footer>
